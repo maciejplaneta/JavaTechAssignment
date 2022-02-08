30ff8edd427cf1c3636bdf16da23f428f00d8781
Maciej <mplaneta@o2.pl>
2022-02-08 22:28:58 +0100

    initial commit

---
+++ src/main/java/com/example/javatechassignment/JavaTechAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatechassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTechAssignmentApplication.class, args);
+    }
+
+}
